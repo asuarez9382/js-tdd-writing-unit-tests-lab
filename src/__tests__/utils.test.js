32ca8e46696cbb06086baa293acebf1e59d56133
Adrian Suarez <asuarez9382@gmail.com>
2024-02-07 17:57:17 -0800

    complete lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,24 @@
 // Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", ()=> {
+    it("returns true if the word is a palindrome and false if not", ()=> {
+        const word="racecar"
+        
+
+        const result=isPalindrome(word)
+        
+        expect(result).toBe(true)
+        
+    })
+
+    it("returns true if the word is a palindrome and false if not", ()=> {
+        
+        const word = "car"
+
+        
+        const result= isPalindrome(word)
+        
+        expect(result).toBe(false)
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,16 @@
 // Your code here
+export function isPalindrome(word) {
+    const splitWord = word.split("")
+
+    const reverseWord = splitWord.reverse().join("")
+    console.log(reverseWord)
+    if(word === reverseWord){
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+isPalindrome("word")
+
